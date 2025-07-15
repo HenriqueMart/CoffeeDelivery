62f9b95a6f8307c6a70567ff760af06880a36e9e
DevMartins <h.martins.silva2014@gmail.com>
2025-07-15 12:20:11 -0300

    Init: struct Project

---
+++ src/layouts/DefaultLayout/index.tsx
@@ -0,0 +1,12 @@
+import {LayoutContainer} from './style'
+
+export function DefaultLayout(){
+    return(
+        <LayoutContainer>
+        <>
+            <h1>Header</h1>
+        </>
+        
+        </LayoutContainer>
+    )
+}
---
+++ src/pages/Home/Index.tsx
@@ -0,0 +1,10 @@
+
+
+
+export function Home(){
+    return(
+        <>
+            <h1>Início do Projeto</h1>
+        </>
+    )
+}
