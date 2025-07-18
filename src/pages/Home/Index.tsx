43c060217a64203e4e01ec2aff088a1eb3bf9b6d
DevMartins <h.martins.silva2014@gmail.com>
2025-07-18 18:32:32 -0300

    Feat: add new main in page Home with resposive

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,10 +1,32 @@
-
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react';
+import banner_coffee from '../../assets/home/Imagem.svg';
+import { Description, IconeDescrition, Main, ProductDescription } from './Styled';
 
 
 export function Home(){
     return(
-        <>
-            <h1>Início do Projeto</h1>
-        </>
+        <Main>
+            <article>
+                <div>
+                    <Description>
+                        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+                        <p>Com coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
+                    </Description>
+                    <ProductDescription>
+                    <div>
+                        <p><IconeDescrition statusColor="yellow_dark"><ShoppingCart size={16} weight='fill' color='white' /></IconeDescrition> Compra simples e segura</p>
+                        <p><IconeDescrition statusColor="yellow"><Timer size={16} weight='fill' color='white'/></IconeDescrition> Entrega rápido e rastreada</p>
+                    </div>
+                    <div>
+                        <p><IconeDescrition statusColor="base_title"><Package size={16} weight='fill' color='white'/></IconeDescrition> Embalagem mantém o café intacto</p>
+                        <p><IconeDescrition statusColor="purple"><Coffee size={16} weight='fill' color='white'/></IconeDescrition> O café chega fresquinho até você</p>
+                    </div>
+                    </ProductDescription>
+                </div>
+                <figure>
+                    <img src={banner_coffee}/>
+                </figure>
+            </article>
+        </Main>
     )
 }
--- src/pages/Home/Styled.ts
+++ src/pages/Home/Styled.ts
@@ -1,9 +1,135 @@
 import styled from "styled-components";
 
-export const Header = styled.div`
+export const Main = styled.main`
+    background-color: ${props => props.theme.background};
+    height: 544px;
+    font-family: 'baloo 2', sans-serif; 
 
+    article{
+        margin: 50px 100px;
+        padding: 25px;
+        display: flex; 
+        gap: 2rem;
+        
+    }
+    figure{
+        img{
+            height: auto;
+        }
+    }
+    @media(max-width: 768px) {
+        article{
+            margin: 20px;
+            padding: 10px;
+            flex-direction: column;
+            align-items: center;
+            flex-wrap: wrap;
+        }
+        figure{
+            width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            img{
+                width: 70%;
+            }
+        }
+        
+    }
+`
+
+export const Description = styled.div`  
+    display: flex;
+    flex-direction: column;
+    
+    gap: 25px;
+    height: 50%;
     h1{
-        background-color: rebeccapurple;
+        width: 90%;
+        color: ${props => props.theme.base_title};
+        font-size: 2.5rem;
+        line-height: 3rem;
+        font-weight: bolder;
+    }
+    p{
+        width: 90%;
+        margin-bottom: 100px;
+        font-size: 1rem;
+        font-weight: 400;
+        color: ${props => props.theme.base_subtitle};
+    }
+    @media(max-width: 768px){
+        text-align: center;
+        align-items: center;
+        justify-content: center;
+        h1{
+            font-size:1.5rem;
+            line-height: 1.5rem;
+        }
+        p{
+            font-size: 0.875rem;
+            margin-bottom: 10px;
+        }
     }
 
 `
+
+export const ProductDescription = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: left;
+    height: 40%;
+    gap: 2rem;
+    div{
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        p{
+            font-size: 0.875rem;
+            color: ${props => props.theme.base_subtitle};
+        }
+        
+    }
+    @media(max-width: 768px){
+        margin-top: 25px;
+        gap: 1rem;
+        height: 100px;
+        div{
+            flex-direction: row;
+            gap: 1rem;
+            width: 100%;
+            p{
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                text-align: center;
+                height: 100%;
+            }
+        }
+    }
+`
+
+interface IconeProps {
+    statusColor: 'yellow_dark'| 'yellow' | 'purple' | 'base_title';
+}
+
+export const IconeDescrition = styled.span<IconeProps>`
+    display: inline-block;/*Centralizando os ícones*/
+    height: 40%;
+    line-height: 40%;
+
+    border-radius: 50%;
+    padding: 5px;
+
+    background-color: ${props => props.theme[props.statusColor]};
+
+    @media(max-width: 768px){
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    
+    }
+`
+
+
